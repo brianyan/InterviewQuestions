f21eaf052c5bff02a9d75c8f338689804495583e
Brian Yan <score14@gmail.com>
2015-10-24 00:36:34 -0700

    Added returnNonDuplicate

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int [] array = {2,5,5,3,2,3,10};
+        System.out.println(returnNonDupe(array));
+
     }
+
+    /* Given an array of duplicates, find the value
+     that is the only nonduplicate  */
+    public static int returnNonDupe(int [] nums){
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length; i++){
+            if(map.get(nums[i])==null){
+                map.put(nums[i],1);
+            }
+            else
+                map.put(nums[i],2);
+        }
+        for(int key: map.keySet()){
+            if(map.get(key) == 1)
+                return key;
+            }
+        return -1;
+}
 }
---
+++ src/StringsandArrays.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by brianyan on 10/23/15.
+ */
+public class StringsandArrays {
+    // Remove duplicates from a list
+    public int [] removeDuplicates(int [] nums){
+
+        return nums;
+    }
+    // Generate all of the permutations of a string
+    public String generatePermuatation(String str){
+
+        return str;
+    }
+    public int returnNonDupe(int [] nums){
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length; i++){
+            if(map.get(nums[i])==null){
+                map.put(nums[i],1);
+            }
+            else
+                map.put(nums[i],2);
+
+        }
+            for(int value: map.values()){
+                if(value == 1){
+                    return value;
+                }
+            }
+        return -1;
+    }
+
+}
